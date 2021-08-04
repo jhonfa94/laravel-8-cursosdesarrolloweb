c3e324e64298743c92cace66a9a94e3bb60cbdb9
Jhon Fabio Cardona Martinez <fabio.cardona@coytex.com.co>
2021-08-04 07:22:44 -0500

    Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +16,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call([
+            PostSeeder::class
+        ]);
+
     }
 }
